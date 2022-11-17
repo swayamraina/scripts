e4700b79b7f8dd165653fc7bc66a8b748cbc540e
Swayam Raina <swayamraina@gmail.com>
2022-11-17 21:09:57 +0530

    script to see which file is common in two folders

---
+++ file-check.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+check_folder='/Users/swayam.raina/swiggy/instamart/dash-fulfilment'
+overall_folder='/Users/swayam.raina/swiggy/instamart/dash-fulfilment/conf'
+
+for file1 in "$check_folder/"*; do
+    found=false
+    for file2 in "$overall_folder/"*; do
+        if cmp -s "$(basename ${file1})" "$(basename ${file2})"; then
+            found=true
+        fi
+    done
+    if [[ "$found" = true ]]; then
+        echo $(basename ${file1})
+    fi
+done
+
